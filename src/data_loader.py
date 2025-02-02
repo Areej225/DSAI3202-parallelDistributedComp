7c2fef698d537d8d02d6e542505c53c2e80951e5
Areej Ali <60103208@udst.edu.qa>
2025-02-02 13:06:24 +0300

    committing

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,13 +1,13 @@
 import pandas as pd
-
-def load_data():
-"""
-Loads the Iris dataset from an online source.
 	
-Returns:
-pandas.DataFrame: The loaded dataset containing features and labels.
-"""
-    url=
-"https://raw.githubusercontent.com/mwaskom/seaborndata/master/iris.csv"
-    data = pd.read_csv(url)
-    return data
+def load_data():
+	    """
+	    Loads the Iris dataset from an online source.
+	
+	    Returns:
+	        pandas.DataFrame: The loaded dataset containing features and labels.
+	    """
+	    url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+	    data = pd.read_csv(url)
+	    return data
+
