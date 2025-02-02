8dc759fad8159bc7518680b8648133c4228763f6
Areej Ali <60103208@udst.edu.qa>
2025-02-02 13:00:43 +0300

    committing

--- src/.ipynb_checkpoints/data_loader-checkpoint.py
+++ src/.ipynb_checkpoints/data_loader-checkpoint.py
@@ -7,6 +7,7 @@ Loads the Iris dataset from an online source.
 Returns:
 pandas.DataFrame: The loaded dataset containing features and labels.
 """
-url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
-data = pd.read_csv(url)
-return data
+    url = "https://raw.githubusercontent.com/mwaskom/seaborn
+    data/master/iris.csv"
+    data = pd.read_csv(url)
+    return data
