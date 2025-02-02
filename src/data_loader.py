22a39b22f37812baed40786d08488ad867b760ee
Areej Ali <60103208@udst.edu.qa>
2025-02-02 13:03:20 +0300

    committing

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,7 +7,7 @@ Loads the Iris dataset from an online source.
 Returns:
 pandas.DataFrame: The loaded dataset containing features and labels.
 """
-    url = "https://raw.githubusercontent.com/mwaskom/seaborn
-    data/master/iris.csv"
+    url=
+"https://raw.githubusercontent.com/mwaskom/seaborndata/master/iris.csv"
     data = pd.read_csv(url)
     return data
