47fddd5665dd1b742960f266ab312808482eb0ca
Areej Ali <60103208@udst.edu.qa>
2025-02-02 12:53:53 +0300

    committing

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,3 +1,8 @@
+from src.data_loader import load_data
+from src.preprocessing import split_data
+from src.model import train_model
+from src.evaluation import evaluate_model
+
 data = load_data()
 X_train, X_test, y_train, y_test = split_data(data)
 model = train_model(X_train, y_train)
